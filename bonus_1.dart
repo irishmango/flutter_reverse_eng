df991e4f4b55bf71b310fa3f5b12407ddbed2eaf
Shokri Francis Raoof <raoofs.de@gmail.com>
2025-03-28 11:41:30 +0100

    first commit

---
+++ bonus_1.dart
@@ -0,0 +1,60 @@
+// MaterialApp(
+//       home: Scaffold(
+//         body: Center(
+//           child: Column(
+//             children: [
+//               Text("Hallo"),
+//               Text("Welt"),
+//             ],
+//           ),
+//         ),
+//       ),
+//     );
+
+abstract class Widget {
+
+}
+
+class MaterialApp extends Widget {
+  final Widget home;
+
+  MaterialApp({required this.home});
+}
+
+class Scaffold extends Widget {
+  final Widget body;
+
+  Scaffold({required this.body});
+}
+
+class Center extends Widget {
+  final Widget child;
+
+  Center({required this.child});
+}
+
+class Column extends Widget {
+  final Widget children;
+
+  Column({required this.children});
+}
+
+class Text extends Widget {
+  final String data;
+
+  Text(this.data);
+}
+
+
+
+final Widget myApp = MaterialApp(
+  home: Scaffold(
+    body: Center(
+      child: Column(
+        children:
+          Text("Hallo"),
+          Text("Welt"),
+        ),
+      ),
+    ),
+  );
